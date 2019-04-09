bacba2de6437e1766b4bbedd22d624f939139ca9
barrerya <barrerya@sheridancollege.ca>
2019-04-09 14:37:18 -0400

    added starting print statement

--- blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
+++ blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
@@ -1,7 +1,10 @@
 package ca.sheridancollege.project;
 
 public class BlackJackGame {
+	
 	public static void main(String []args) {
+		System.out.print("Do you want to start a bame of 21? ");
+		
 		
 	}
 }
