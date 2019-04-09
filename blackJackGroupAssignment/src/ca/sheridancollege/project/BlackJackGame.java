0b7374d4da3bd3a561c031ba5b200ee90735f8cb
barrerya <barrerya@sheridancollege.ca>
2019-04-09 14:43:38 -0400

    fixed a typo

--- blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
+++ blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.project;
 public class BlackJackGame {
 	
 	public static void main(String []args) {
-		System.out.print("Do you want to start a bame of 21? ");
+		System.out.print("Do you want to start a game of 21? ");
 		
 		
 	}
