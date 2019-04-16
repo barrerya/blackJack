1a68bb95bf39455d2e876b1ce53ddd912b7b970f
barrerya <barrerya@sheridancollege.ca>
2019-04-16 13:16:46 -0400

    added some game functionality, static method issues need to be fixed.

--- blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
+++ blackJackGroupAssignment/src/ca/sheridancollege/project/BlackJackGame.java
@@ -1,10 +1,26 @@
 package ca.sheridancollege.project;
 
+import java.util.Scanner;
+
 public class BlackJackGame {
-	
+
+	Game curentGame = new Game();
 	public static void main(String []args) {
+		Scanner input = new Scanner(System.in);
+		String response;
 		System.out.print("Do you want to start a game of 21? ");
+		response = input.nextLine();
 		
+		if (response.equals("yes") || response.equals("Yes"))
+		{
+			
+			curentGame.play();
+		}
+		else
+		{
+			System.out.println("I see");
+
+		}
 		
 	}
 }
--- blackJackGroupAssignment/src/ca/sheridancollege/project/Game.java
+++ blackJackGroupAssignment/src/ca/sheridancollege/project/Game.java
@@ -6,59 +6,55 @@
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
  * @author dancye, 2018
  */
-public abstract class Game 
+public class Game 
 {
-    private final String gameName;//the title of the game
-    private ArrayList <Player> players;// the players of the game
-    
-    public Game(String givenName)
+	//private final String gameName;//the title of the game
+    private ArrayList<Player> players = new ArrayList<Player>(); // the players of the game
+    static Scanner input = new Scanner(System.in);
+    public Game()
     {
-        gameName = givenName;
+        //gameName = givenName;
         players = new ArrayList();
     }
 
-    /**
-     * @return the gameName
-     */
-    public String getGameName() 
-    {
-        return gameName;
-    }
-    
-     /**
-     * @return the players of this game
-     */
+
     public ArrayList <Player> getPlayers() 
     {
         return players;
     }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList <Player> players) 
+
+    public void setPlayers(ArrayList <Player> players)
     {
         this.players = players;
     }
-    
-    /**
-     * Play the game. This might be one method or many method calls depending
-     * on your game.
-     */
-    public abstract void play();
-    
-    /**
-     * When the game is over, use this method to declare and display a winning
-     * player.
-     */
-    public abstract void declareWinner();
 
+
+    public void play()
+    {
+        String addPlayers = "";
+        while (!(addPlayers.equals("stop")))
+        {
+            System.out.println("What is the player's name?");
+            players.add(new Player(input.nextLine()));
+            System.out.println("Would you like to add another player? type 'stop' for no, type anything else to continue");
+            addPlayers = input.nextLine();
+
+        }
+        System.out.println("Players: " );
+    }
+
+    public void declareWinner()
+    {
+
+    }
    
     
 }//end class
